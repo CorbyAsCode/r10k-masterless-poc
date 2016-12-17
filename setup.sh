9e4c810caec7792fb8ce78685c4719760cdb8682
Corby Shaner <corbyshaner@gmail.com>
2016-12-17 16:58:53 -0500

    Moved bundle export to its correct place

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ yum install centos-release-scl -y
 yum install ruby200-scldevel ruby200-ruby -y
 source /opt/rh/ruby200/enable
 gem install bundler --no-ri --no-rdoc
+# Export the path to bundler
+export PATH=/opt/rh/ruby200/root/usr/local/bin:$PATH
 bundle install --path=setup/bundle --binstubs=setup/bundlebin
 
 # Alternative method for installing gems
@@ -15,9 +17,6 @@ bundle install --path=setup/bundle --binstubs=setup/bundlebin
 #gem install rake --no-ri --no-rdoc
 #gem install librarian-puppet --no-ri --no-rdoc
 #gem install r10k --no-ri --no-rdoc
-
-# Export the path to bundler
-export PATH=/opt/rh/ruby200/root/usr/local/bin:$PATH
 
 # get environment from current git branch
 environment=$(git symbolic-ref HEAD | awk -F/ '{print $3}')
