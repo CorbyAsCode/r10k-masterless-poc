4cdc5e65840d1e21a2f50cf00578d6351b8078a2
Corby Shaner <corbyshaner@gmail.com>
2016-12-17 16:34:36 -0500

    Added the 'bundle' install method

--- setup.sh
+++ setup.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 set -x
+set -e
 
-#dir="$(dirname $(readlink -f $0))"
-#cd $dir
-
-# git pull && git submodule update --init
-#if [ ! -f Gemfile.lock ]; then
-#first run
-  #apt-get update
-  #apt-get install -y ruby ruby-dev make git
-  #gem install bundler
-  #bundle install --path=setup/bundle --binstubs=setup/bundlebin
-#fi
 yum install centos-release-scl -y
 yum install ruby200-scldevel ruby200-ruby -y
 source /opt/rh/ruby200/enable
-gem install puppet -v '3.8.2' --no-ri --no-rdoc
-gem install puppet-lint --no-ri --no-rdoc
-gem install puppetlabs_spec_helper --no-ri --no-rdoc
-gem install rake --no-ri --no-rdoc
-gem install librarian-puppet --no-ri --no-rdoc
-gem install r10k --no-ri --no-rdoc
+gem install bundler
+bundle install --path=setup/bundle --binstubs=setup/bundlebin
 
-export PATH=/opt/rh/ruby200/root/usr/local/bin:$PATH
+# Alternative method for installing gems
+#gem install puppet -v '3.8.2' --no-ri --no-rdoc
+#gem install puppet-lint --no-ri --no-rdoc
+#gem install puppetlabs_spec_helper --no-ri --no-rdoc
+#gem install rake --no-ri --no-rdoc
+#gem install librarian-puppet --no-ri --no-rdoc
+#gem install r10k --no-ri --no-rdoc
+
+# Export the path to the gems that weren't installed using bundle
+#export PATH=/opt/rh/ruby200/root/usr/local/bin:$PATH
 
 # get environment from current git branch
 environment=$(git symbolic-ref HEAD | awk -F/ '{print $3}')
-#environment=$1
 
 # ssh wrapper to use custom sshkey for git
 export GIT_SSH="setup/git_ssh"
@@ -36,10 +30,13 @@ chmod 600 setup/.ssh/puppet_rsa
 
 # deploy Puppetmodules and hieradata
 # deploys from remote git repository!
-#setup/bundlebin/r10k deploy environment $environment
-r10k deploy environment $environment
+setup/bundlebin/r10k deploy environment $environment
+# Use this method for r10k if you used the alternative method
+# for installing the gems
+#r10k deploy environment $environment
 
 # Run Puppet
-#setup/bundlebin/puppet apply --modulepath=/etc/puppet/environments/$environment/modules --hiera_config=hiera.yaml --environment $environment site.pp $@
-puppet apply --modulepath=/etc/puppet/environments/$environment/modules --hiera_config=hiera.yaml --environment $environment site.pp $@
-
+setup/bundlebin/puppet apply --modulepath=/etc/puppet/environments/$environment/modules --hiera_config=hiera.yaml --environment $environment site.pp $@
+# Use this method for r10k if you used the alternative method
+# for installing the gems
+#puppet apply --modulepath=/etc/puppet/environments/$environment/modules --hiera_config=hiera.yaml --environment $environment site.pp $@
