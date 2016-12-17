949062d7d3f1cd66c830bc44ddf6864a0e08776c
Corby Shaner <corbyshaner@gmail.com>
2016-12-16 20:53:40 -0500

    Updated script

--- setup.sh
+++ setup.sh
@@ -5,16 +5,19 @@ dir="$(dirname $(readlink -f $0))"
 cd $dir
 
 # git pull && git submodule update --init
-if [ ! -f Gemfile.lock ]; then
+#if [ ! -f Gemfile.lock ]; then
 #first run
-  apt-get update
-  apt-get install -y ruby ruby-dev make git
-  gem install bundler
-  bundle install --path=setup/bundle --binstubs=setup/bundlebin
-fi
+  #apt-get update
+  #apt-get install -y ruby ruby-dev make git
+  #gem install bundler
+  #bundle install --path=setup/bundle --binstubs=setup/bundlebin
+#fi
+rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+yum install puppet -y
 
 # get environment from current git branch
-environment=$(git symbolic-ref --short HEAD)
+#environment=$(git symbolic-ref --short HEAD)
+environment=$1
 
 # ssh wrapper to use custom sshkey for git
 export GIT_SSH="${dir}/setup/git_ssh"
@@ -24,7 +27,7 @@ chmod 600 $dir/setup/.ssh/puppet_rsa
 
 # deploy Puppetmodules and hieradata
 # deploys from remote git repository!
-setup/bundlebin/r10k deploy environment $environment
+#setup/bundlebin/r10k deploy environment $environment
 
 # Run Puppet
-setup/bundlebin/puppet apply --modulepath=/etc/puppet/environments/$environment/modules --hiera_config=hiera.yaml --environment $environment site.pp $@
+#setup/bundlebin/puppet apply --modulepath=/etc/puppet/environments/$environment/modules --hiera_config=hiera.yaml --environment $environment site.pp $@
