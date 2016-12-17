242b949f3e5ce1f6faa424d0c680778e10db7a65
Corby Shaner <corbyshaner@gmail.com>
2016-12-17 15:56:30 -0500

    added changes that hopefully work

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x
 
-dir="$(dirname $(readlink -f $0))"
-cd $dir
+#dir="$(dirname $(readlink -f $0))"
+#cd $dir
 
 # git pull && git submodule update --init
 #if [ ! -f Gemfile.lock ]; then
@@ -12,13 +12,20 @@ cd $dir
   #gem install bundler
   #bundle install --path=setup/bundle --binstubs=setup/bundlebin
 #fi
-rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
-#yum localinstall /var/tmp/ruby-1.9.3p545-1.el6.x86_64.rpm -y
-#yum install puppet -y
-gem install r10k
+yum install centos-release-scl -y
+yum install ruby200-scldevel ruby200-ruby -y
+source /opt/rh/ruby200/enable
+gem install puppet -v '3.8.2' --no-ri --no-rdoc
+gem install puppet-lint --no-ri --no-rdoc
+gem install puppetlabs_spec_helper --no-ri --no-rdoc
+gem install rake --no-ri --no-rdoc
+gem install librarian-puppet --no-ri --no-rdoc
+gem install r10k --no-ri --no-rdoc
+
+export PATH=/opt/rh/ruby200/root/usr/local/bin:$PATH
 
 # get environment from current git branch
-environment=$(git symbolic-ref --short HEAD)
+environment=$(git symbolic-ref HEAD | awk -F/ '{print $3}')
 #environment=$1
 
 # ssh wrapper to use custom sshkey for git
@@ -31,5 +38,8 @@ chmod 600 $dir/setup/.ssh/puppet_rsa
 # deploys from remote git repository!
 #setup/bundlebin/r10k deploy environment $environment
 r10k deploy environment $environment
+
 # Run Puppet
 #setup/bundlebin/puppet apply --modulepath=/etc/puppet/environments/$environment/modules --hiera_config=hiera.yaml --environment $environment site.pp $@
+puppet apply --modulepath=/etc/puppet/environments/$environment/modules --hiera_config=hiera.yaml --environment $environment site.pp $@
+
